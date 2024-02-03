1624449085116d1a78089d6df2dfe32214e50a4a
Ruslan Zarlykov <ruslionkz@gmail.com>
2024-02-03 16:16:02 +0500

    updated

--- model_design.py
+++ model_design.py
@@ -17,21 +17,21 @@ def model_design(EPOCHS,  x_train, y_train, x_test, y_test, class_weights_dict):
     x = normal_layer(inputs)
 
     
-    x = layers.LSTM(num_features * 4, activation="relu", return_sequences=True)(x)
-    x = layers.LSTM(num_features * 3, activation="relu", return_sequences=True)(x)
-    x = layers.LSTM(num_features * 2, activation="relu", return_sequences=True)(x)
     x = layers.LSTM(num_features, activation="relu", return_sequences=True)(x)
-    x = layers.LSTM(num_features // 2, activation="relu", return_sequences=True)(x)
-    x = layers.LSTM(num_features // 4, activation="relu", return_sequences=True)(x)
-    x = layers.LSTM(num_features // 8, activation="relu", return_sequences=True)(x)
-    x = layers.LSTM(num_features // 16, activation="relu")(x)
+    x = layers.LSTM(num_features // 8, activation="relu")(x)
+    # x = layers.LSTM(num_features * 2, activation="relu", return_sequences=True)(x)
+    # x = layers.LSTM(num_features, activation="relu", return_sequences=True)(x)
+    # x = layers.LSTM(num_features // 2, activation="relu", return_sequences=True)(x)
+    # x = layers.LSTM(num_features // 4, activation="relu", return_sequences=True)(x)
+    # x = layers.LSTM(num_features // 8, activation="relu", return_sequences=True)(x)
+    # x = layers.LSTM(num_features // 16, activation="relu")(x)
 
         
-    x = layers.Flatten() (x)
+    
     x = layers.Dense(x.shape[1], activation="relu")(x)
+    x = layers.Dense(4, activation="relu")(x)
     
-    
-    
+     
     
     output_layer = layers.Dense(len(class_weights_dict), name='output', activation='softmax' ) (x)
     # output_layer = layers.Dense(1, name='output', activation='softmax') (x)
