ede25785566cc7974b1a329c03cf06f7452ea7f9
Ruslan <ruslionkz@gmail.com>
2024-07-23 20:42:18 +0000

    Secret key changed

--- my_vps_project/my_vps_project/settings.py
+++ my_vps_project/my_vps_project/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zk4iob5p+*a2o5a8p)9h@6*l&yu_wmma&7w_ol)r9o%1kn8*3i'
+
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
