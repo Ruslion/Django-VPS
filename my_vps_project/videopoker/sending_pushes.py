9971d4d8483982fffc5c75e4175de34b09cc8d67
Ruslan <ruslionkz@gmail.com>
2024-10-02 11:45:12 +0000

    loggin added

--- my_vps_project/videopoker/sending_pushes.py
+++ my_vps_project/videopoker/sending_pushes.py
@@ -22,7 +22,8 @@ class AsyncRateLimitedRequester:
             async with aiohttp.ClientSession() as session:
                 params['chat_id'] = tg_id
                 async with session.get(url, params=params) as response:
-                    print(f"Request to {url} completed with status code: {response.status}")
+                    if response.status != 200:
+                        print(f"Request to {url} completed with status code: {response.status}. User id {tg_id}")
             
             self.last_request_time = time.time()
 
@@ -42,21 +43,24 @@ async def main():
     telegram_id_list = database_connect.execute_select_sql(selection_sql, None)
 
     inline_keyboard_dict = {'inline_keyboard': [[{'text': 'Play!',
-                                    'web_app': {'url': 'https://pychampion.site/videopoker'}
+                                    'web_app': {'url': 'https://pychampion.site/videopoker/'}
                                   }]]}
 
     reply_markup = json.dumps(inline_keyboard_dict)
 
     params = {'chat_id': 0,
           'parse_mode':'HTML',
-          'text': '''Hey, video poker fans! 👋
+          'text': '''🃏 Video Poker Fans! Ready for a royal challenge? 👑
           
-Did you know that 24,729 hands have been played since we launched, and still no one has hit a <b>Royal Flush</b> 👑?
+Did you know there are 2,598,960 unique hand combinations in 5-card draw? Yet, the elusive Royal Flush appears only once in 649,740 hands on average!
 
-Could you be the lucky one to make history and win some prize?
+Can you beat the odds? Our game uses virtual chips, so it's all about skill, no financial risk!
 
-Running low on chips? No worries!
-You can easily replenish your stack by sharing your referral link with a friend. Just tap the chips icon to find your unique link. The game is still evolving, stay tuned and keep playing!''',
+🏆 In the leaderboard you may find who took the first row for the last week and month.
+
+Will you top the leaderboard this week? Login now and show us your poker face!
+Remember, every hand could be that 1 in 649,740.
+Good luck!''',
           'disable_notification': 'true',
           'reply_markup':reply_markup
           }
